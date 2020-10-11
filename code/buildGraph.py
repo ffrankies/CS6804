ea2e0e8ff4ef14438c2d0c8c9a8318155c6bf4a3
Bipasha-banerjee <bips.ban@gmail.com>
2020-10-11 16:28:06 -0400

    bug in evaluation of label propagation

--- code/buildGraph.py
+++ code/buildGraph.py
@@ -2,11 +2,12 @@ import networkx as nx
 import pandas as pd
 import itertools
 import copy
+import numpy
 import matplotlib.pyplot as plt
 import sys
 import csv
 from networkx.algorithms import community
-from code.evaluateGraph import *
+from evaluateGraph import *
 
 tsv_file = open("/Users/bipashabanerjee/Documents/CS/sem5/SGML/CS6804/cora/cora_graph.tsv")
 # read_tsv = csv.reader(tsv_file, delimiter="\t")
@@ -21,8 +22,11 @@ for i, elrow in edgelist.iterrows():
 
 lp = list(community.label_propagation_communities(graph))
 lpcoms = [tuple(x) for x in lp]
-
+algo_graph_nparray = numpy.array(lpcoms)
 #To run the evaluation script
 
-true_tsv_file = open("/Users/bipashabanerjee/Documents/CS/sem5/SGML/CS6804/cora/cora_graph.tsv")
-evaluate_partition()
+true_tsv_df = pd.read_csv("/Users/bipashabanerjee/Documents/CS/sem5/SGML/CS6804/cora/cora_truePartition.tsv",sep="\t")
+true_value_nparray = true_tsv_df.values
+# print(type(true_tsv_df.values))
+
+evaluate_partition(true_value_nparray,algo_graph_nparray)
