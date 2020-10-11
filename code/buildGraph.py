d505078b212d652d244c643b04a815d392c47ae9
Bipasha-banerjee <bips.ban@gmail.com>
2020-10-11 15:42:26 -0400

    using label propagation

--- code/buildGraph.py
+++ code/buildGraph.py
@@ -5,6 +5,7 @@ import copy
 import matplotlib.pyplot as plt
 import sys
 import csv
+from networkx.algorithms import community
 
 tsv_file = open("/Users/bipashabanerjee/Documents/CS/sem5/SGML/CS6804/cora/cora_graph.tsv")
 # read_tsv = csv.reader(tsv_file, delimiter="\t")
@@ -16,3 +17,7 @@ graph = nx.Graph()
 
 for i, elrow in edgelist.iterrows():
     graph.add_edge(elrow[0], elrow[1], attr_dict=elrow[2:].to_dict())
+
+lp = list(community.label_propagation_communities(graph))
+lpcoms = [tuple(x) for x in lp]
+print(lpcoms)
