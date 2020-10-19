7376b394d40413210ac4b0912092415dbcaeb279
Bipasha-banerjee <bips.ban@gmail.com>
2020-10-18 21:16:23 -0400

    update

--- code/buildGraph.py
+++ code/buildGraph.py
@@ -13,20 +13,41 @@ tsv_file = open("/Users/bipashabanerjee/Documents/CS/sem5/SGML/CS6804/cora/cora_
 # read_tsv = csv.reader(tsv_file, delimiter="\t")
 # for row in read_tsv:
 #   print(row)
-
+output_file = open("/Users/bipashabanerjee/Documents/CS/sem5/SGML/CS6804/cora/output.txt","a")
 edgelist = pd.read_csv(tsv_file, delimiter="\t")
 graph = nx.Graph()
 
 for i, elrow in edgelist.iterrows():
-    graph.add_edge(elrow[0], elrow[1], attr_dict=elrow[2:].to_dict())
+    graph.add_edge(elrow[0], elrow[1])
 
 lp = list(community.label_propagation_communities(graph))
 lpcoms = [tuple(x) for x in lp]
-algo_graph_nparray = numpy.array(lpcoms)
-#To run the evaluation script
+i = 0
+# for list_1 in lpcoms:
+  
+#     for l in list_1:
+#         # print(type(l))
+#         #rint(str(l) + "\t"+ str(i))
+#        # output_file.write(str(l) + "\t"+ str(i)+"\n")
+#     i = i +1 
+#Read the file and sort based on the node number
+algo_graph_list=[]
+lines = open('/Users/bipashabanerjee/Documents/CS/sem5/SGML/CS6804/cora/output.txt', 'r').readlines()
+for line in sorted(lines, key=lambda line: line.split()[0]):
+    # print(line.split('\t')[1])
+    algo_graph_list.append(line.split('\t')[1].strip())
+algo_graph_nparray= np.array(algo_graph_list)
+# algo_graph_nparray = numpy.array(lpcoms)
+# #To run the evaluation script
+arr=[]
 
-true_tsv_df = pd.read_csv("/Users/bipashabanerjee/Documents/CS/sem5/SGML/CS6804/cora/cora_truePartition.tsv",sep="\t")
-true_value_nparray = true_tsv_df.values
-# print(type(true_tsv_df.values))
+with open("/Users/bipashabanerjee/Documents/CS/sem5/SGML/CS6804/cora/cora_truePartition.tsv","r") as csv_file:
+    true_pred = list(csv.reader(csv_file, delimiter="\t"))
+    for each_row in true_pred:
+
+        arr.append(str(int(each_row[1])-1))
+    true_value_nparray = np.array(arr)
+
+
 
 evaluate_partition(true_value_nparray,algo_graph_nparray)
